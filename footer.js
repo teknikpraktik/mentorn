4d9428144399c6eed29e195fb531f062a875ed5c
teknikpraktik <per.a.bjorkman@gmail.com>
2026-05-26 12:25:12 +0200

    Ändrat kontaktinfo och copyright i footer

--- footer.js
+++ footer.js
@@ -1,21 +1,21 @@
 (function () {
-  const isIndex = window.location.pathname.endsWith('index.html') 
+  const isIndex = window.location.pathname.endsWith('index.html')
     || window.location.pathname.endsWith('/');
   if (!isIndex) return;
 
   const footer = document.createElement('footer');
   footer.className = 'site-footer';
   footer.innerHTML = `
-    <p class="footer-copy">&copy; 2026 Per Björkman</p>
-    <a class="footer-contact" href="om.html">Om Mentorn</a>
-    <a class="footer-contact" href="mailto:per.a.bjorkman@gmail.com">Kontakta mig</a>
-  `;
+  <a class="footer-contact" href="om.html">Om Mentorn</a>
+  <a class="footer-contact" href="mailto:per.a.bjorkman@gmail.com">Kontakta mig</a>
+  <p class="footer-copy">&copy; 2026 Per Björkman</p>
+`;
   const wrapper = document.querySelector('.page-wrapper');
   if (wrapper) wrapper.appendChild(footer);
 })();
 
 // Goatcounter
-(function() {
+(function () {
   const script = document.createElement('script');
   script.dataset.goatcounter = 'https://mentorn.goatcounter.com/count';
   script.async = true;
